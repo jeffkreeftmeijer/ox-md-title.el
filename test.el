d06c09e754a9166ea7cd3df2d0b39879d66fb58d
Jeff Kreeftmeijer <jeff@kreeft.me>
2023-04-14 09:22:56 +0200

    Move org-md-title enabling to with-org-md-title in test

--- test.el
+++ test.el
@@ -1,15 +1,18 @@
 (require 'ert)
 (load-file "ox-md-title.el")
 
-(ert-deftest title-test ()
+(defun with-org-md-title (func)
   (org-md-title-add)
+  (funcall func)
+  (org-md-title-remove))
+
+(ert-deftest title-test ()
   (find-file "ox-md-title.org")
-  (org-md-export-as-markdown)
+  (with-org-md-title #'org-md-export-as-markdown)
 
   (let ((buffer (with-current-buffer
 		    "*Org MD Export*"
 		  (buffer-string))))
 
     (should (string-match-p "^# ox-md-title" buffer))
-    (should (string-match-p "^## Usage" buffer)))
-  (org-md-title-remove))
+    (should (string-match-p "^## Usage" buffer))))
