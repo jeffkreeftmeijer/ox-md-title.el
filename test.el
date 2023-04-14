76cd125906154620ca15a0524a85e40e67a48fde
Jeff Kreeftmeijer <jeffkreeftmeijer@gmail.com>
2023-04-14 17:15:45 +0200

    Add org-md-title option to enable titles (#1)
    * Move org-md-title enabling to with-org-md-title in test
    
    * Add failing test for the org-md-title option
    
    * Add org-export-md-title, disable advice when non-nil
    
    * Describe functionality
    
    * Call org-md-title-add in usage example

--- test.el
+++ test.el
@@ -1,15 +1,31 @@
 (require 'ert)
 (load-file "ox-md-title.el")
 
-(ert-deftest title-test ()
+(defun with-org-md-title (func)
   (org-md-title-add)
+  (funcall func)
+  (org-md-title-remove))
+
+(ert-deftest title-test ()
   (find-file "ox-md-title.org")
-  (org-md-export-as-markdown)
+  (let ((org-md-title t))
+    (with-org-md-title #'org-md-export-as-markdown))
 
   (let ((buffer (with-current-buffer
 		    "*Org MD Export*"
 		  (buffer-string))))
 
     (should (string-match-p "^# ox-md-title" buffer))
-    (should (string-match-p "^## Usage" buffer)))
-  (org-md-title-remove))
+    (should (string-match-p "^## Usage" buffer))))
+
+(ert-deftest title-disabled-test ()
+  (find-file "ox-md-title.org")
+  (let ((org-md-title nil))
+    (with-org-md-title #'org-md-export-as-markdown))
+
+  (let ((buffer (with-current-buffer
+		    "*Org MD Export*"
+		  (buffer-string))))
+
+    (should-not (string-match-p "# ox-md-title" buffer))
+    (should (string-match-p "^# Usage" buffer))))
